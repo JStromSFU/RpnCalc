f71cc0f70841cad16db2d396a4d5867365d0cfe6
Joseph Strom <jstrom@francis.edu>
2026-01-11 21:17:22 -0500

    Add calculator state to UI
    Connect digit inputs and arithmetic operations

--- src/rpn/ui.py
+++ src/rpn/ui.py
@@ -1,4 +1,11 @@
+from enum import Enum
 from PySide6.QtWidgets import *
+
+from rpn.calc import RpnCalculator
+
+class UiState(Enum):
+  Insert = 0
+  Edit = 1
 
 class MainWindow(QMainWindow):
   '''
@@ -11,6 +18,11 @@ class MainWindow(QMainWindow):
 
   def __init__(self):
     super().__init__()
+
+    self.rpn = RpnCalculator()
+
+    self.state = UiState.Insert
+    self.value = 0
 
     self.setStyleSheet("""
     * {
@@ -42,14 +54,14 @@ class MainWindow(QMainWindow):
     displayFrameLayout = QGridLayout(displayFrame)
     frameLayout.addWidget(displayFrame)
 
-    self.stackDisplay = []
+    self.stack_display = []
 
     # Stack Display
     for i in range(4):
       displayFrameLayout.addWidget( QLabel( str(i), objectName="Label" ), 3-i, 0 )
       
       display = QLabel( "-", objectName="Display" )
-      self.stackDisplay.append(display)
+      self.stack_display.append(display)
       displayFrameLayout.addWidget( display, 3-i, 1 )
 
     # Control Buttons
@@ -58,18 +70,86 @@ class MainWindow(QMainWindow):
     frameLayout.addWidget(keypadFrame)
 
     # Stack Operations
-    keypadFrameLayout.addWidget( QPushButton( "Enter", objectName="Operator" ), 0, 2, 1, 2 )
+    keypadFrameLayout.addWidget( self.make_operator_button( "Enter", lambda: None ), 0, 2, 1, 2 )
 
     # Numeric Input
     for i in range(9):
-      keypadFrameLayout.addWidget( QPushButton( str(i + 1), objectName="Numpad" ), 2 + i // 3, i % 3 )
+      button = self.make_numpad_button( i + 1 )
+      keypadFrameLayout.addWidget( button, 2 + i // 3, i % 3 )
 
-    keypadFrameLayout.addWidget( QPushButton( "0", objectName="Numpad" ), 5, 0, 1, 2 )
+    zeroButton = self.make_numpad_button( 0 )
+    keypadFrameLayout.addWidget( zeroButton, 5, 0, 1, 2 )
+
     keypadFrameLayout.addWidget( QPushButton( ".", objectName="Numpad" ), 5, 2 )
 
     # Arithmetic operations
-    keypadFrameLayout.addWidget( QPushButton( "/", objectName="Operator" ), 2, 3 )
-    keypadFrameLayout.addWidget( QPushButton( "*", objectName="Operator" ), 3, 3 )
-    keypadFrameLayout.addWidget( QPushButton( "-", objectName="Operator" ), 4, 3 )
-    keypadFrameLayout.addWidget( QPushButton( "+", objectName="Operator" ), 5, 3 )
+    keypadFrameLayout.addWidget( self.make_operator_button("/", self.rpn.div ), 2, 3 )
+    keypadFrameLayout.addWidget( self.make_operator_button("*", self.rpn.mul ), 3, 3 )
+    keypadFrameLayout.addWidget( self.make_operator_button("-", self.rpn.sub ), 4, 3 )
+    keypadFrameLayout.addWidget( self.make_operator_button("+", self.rpn.add ), 5, 3 )
 
+  def make_numpad_button(self, digit):
+    '''
+    Create a button for numeric input
+    '''
+    button = QPushButton( str(digit), objectName="Numpad" )
+    button.clicked.connect( lambda: self.add_digit(digit) )
+    return button
+
+  def make_operator_button( self, text, func ):
+    '''
+    Create a button for an operator
+    '''
+    button = QPushButton( text, objectName="Operator" )
+    button.clicked.connect( lambda: self.do_operation(func) )
+    return button
+
+  def add_digit(self, d):
+    '''
+    Add digit to the currently edited number
+    '''
+    if self.state == UiState.Insert:
+      self.state = UiState.Edit
+      self.value = d
+    else:
+      self.value *= 10
+      self.value += d
+    self.update_display()
+
+  def do_operation(self, func):
+    '''
+    Run an operation,
+    First pushing any edited value onto the stack
+    And after updating the display
+    '''
+    self.push_edit()
+    func()
+    self.update_display()
+
+  def update_display(self):
+    '''
+    Update the stack display
+    '''
+    for i in range(4):
+      # In insert mode show the stack as it exists
+      # In edit mode show the value being edited as if it were
+      # at the top of the stack
+      if self.state == UiState.Insert:
+        v = self.rpn.peek(i)
+      elif i == 0:
+        v = self.value
+      else:
+        v = self.rpn.peek(i - 1)
+
+      if v is None:
+        self.stack_display[i].setText( '-' )
+      else:
+        self.stack_display[i].setText( str(v) )
+
+  def push_edit(self):
+    '''
+    Push the edited value (if any) onto the stack
+    '''
+    if self.state == UiState.Edit:
+      self.rpn.push( self.value )
+      self.state = UiState.Insert
